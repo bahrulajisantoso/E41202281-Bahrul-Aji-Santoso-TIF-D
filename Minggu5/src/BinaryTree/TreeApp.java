7e1dd002eeb3be8130561d5e8af71c30f3e1e6ba
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2021-04-10 23:58:23 +0700

    Add files via upload

---
+++ Minggu5/src/BinaryTree/TreeApp.java
@@ -0,0 +1,87 @@
+package BinaryTree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TreeApp {
+    public static void main (String[] args) throws IOException {
+
+        int value;
+        String data;
+        Tree theTree = new Tree();
+        theTree.insert(50,"a");
+        theTree.insert(25,"b");
+        theTree.insert(75,"c");
+        theTree.insert(12,"d");
+        theTree.insert(37,"e");
+        theTree.insert(43,"f");
+        theTree.insert(30,"g");
+        theTree.insert(33,"h");
+        theTree.insert(87,"i");
+        theTree.insert(93,"j");
+        theTree.insert(97,"k");
+
+        while (true) {
+            System.out.print("Enter first letter of show " +
+                    "insert, find, delete, or traverse: ");
+            int choice = getChar();
+            switch (choice) {
+                case 's' :
+                    theTree.displayTree();
+                    break;
+                case 'i' :
+                    System.out.print("Enter value data to " + "insert: ");
+                    value = getInt();
+                    data = getString();
+                    theTree.insert(value, data);
+                    break;
+                case 'f' :
+                    System.out.print("Enter value and data to " + "find: ");
+                    value = getInt();
+                    Node found = theTree.find(value);
+                    if (found != null) {
+                            System.out.print("Found: ");
+                            found.diplsyNode();
+                            System.out.print("\n");
+                        } else {
+                            System.out.println("Could not find " + value);
+                        }
+                        break;
+                case 'd' :
+                    System.out.print("Enter value to delete: ");
+                    value = getInt();
+                    boolean diDelete = theTree.delete(value);
+                    if (diDelete) {
+                        System.out.println("Deleted " + value);
+                    } else {
+                        System.out.println("Could not delete " + value);
+                    }
+                    break;
+                case 't' :
+                    System.out.print("Enter type 1, 2, or 3: ");
+                    value = getInt();
+                    theTree.traverse(value);
+                    break;
+                default:
+                    System.out.println("Invalid entry ");
+            }
+        }
+    }
+    public static String getString() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return s;
+    }
+    public static char getChar() throws IOException {
+        String s = getString();
+        return s.charAt(0);
+    }
+    public static int getInt() throws IOException {
+        String s = getString();
+        return Integer.parseInt(s);
+
+    }
+}
+
