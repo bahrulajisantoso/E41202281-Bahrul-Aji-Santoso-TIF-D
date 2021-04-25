deb82c85e0dc24743a50ca98d9b7cad0c4dd2849
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2021-04-26 06:09:38 +0700

    Add files via upload

---
+++ Minggu7/televisiModern/mainTvModern.java
@@ -0,0 +1,26 @@
+package televisiModern;
+
+public class mainTvModern {
+    public static void main(String[] args) {
+        TelevisiModern tv = new TelevisiModern("TV KONGUHAN , 12 inc" ,12);
+        
+        String [] semuaChannel = {"RCTI" , "SCTV", "INDOSIAR", "LA TV","METRO TV"
+                                                ,"METRO TV", "TRANS TV", "TPI", "TV7", "TVRI"
+                                                 ,"TV G", "AN TV"};
+
+        System.out.println("Pak Lukman, membeli Televisi : " + tv.getDeskripsi());
+       
+        tv.getChannels();
+        tv.setChannels(semuaChannel);
+        tv.setChannelAktif(3);
+        tv.setVolume(10);
+        tv.setChannelAktif(10);
+        tv.setChannelAktif(1);
+        tv.setModusTampilan(TelevisiModern.TELETEXT);
+        tv.setHalamanTeleText(250); 
+        tv.setModusTampilan(TelevisiModern.tV);
+        tv.playCD();
+        tv.setDiscTray("OVJ");
+        tv.playCD();
+    }
+}
